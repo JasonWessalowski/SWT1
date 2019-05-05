b8090fede1618c8e1bb769ea9f4d117e8c5f1048
Jason Wessalowski Matrikelnummer 2048877 <jasonwessalowski@gmail.com>
2019-05-05 17:27:47 +0200

    Hinzunahme zwei weiterer Tests, ob nach der Rotationen einens Bildes um 90 Grad bzw. 270 Grad die Höhe mit der Breite übereinstimmt bzw. die Breite mit der Höhe des Originals. Zusätzlich wird getested, ob es sich noch um das selbe Bild handelt.

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -51,9 +51,54 @@ public class GeneratorTest {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void rotateImageTestException() {
-			
+
 		generator.rotateImage(bufferedImage, 0.42);
-		
+
 	}
 
+	@Test
+	public void rotateImageTest90() {
+
+		BufferedImage editedImage = generator.rotateImage(bufferedImage, Math.toRadians(90));
+
+		assertEquals("Überprüfe ob Höhe des Bildes vor Drehng mit Breite des gedrehten Bildes übereinstimmt.", bufferedImage.getHeight(), editedImage.getWidth());
+		assertEquals("Überprüfe ob Breite des Bildes vor Drehng mit Höhe des gedrehten Bildes übereinstimmt.", bufferedImage.getWidth(), editedImage.getHeight());
+		
+		for (int i = 0; i < bufferedImage.getWidth(); i++) {
+			
+			for (int j = 0; j < bufferedImage.getHeight(); j++) {
+				
+				int originalPixel = bufferedImage.getRGB(i, j);
+				int editedPixel = editedImage.getRGB(editedImage.getWidth() - 1 - j, i);
+	
+				assertEquals("Stimmen die individuellen Pixel überein?", originalPixel, editedPixel);
+				
+			}
+			
+		}
+
+	}
+	
+	@Test
+	public void rotateImageTest270() {
+
+		BufferedImage editedImage = generator.rotateImage(bufferedImage, Math.toRadians(270));
+
+		assertEquals("Überprüfe ob Höhe des Bildes vor Drehng mit Breite des gedrehten Bildes übereinstimmt.", bufferedImage.getHeight(), editedImage.getWidth());
+		assertEquals("Überprüfe ob Breite des Bildes vor Drehng mit Höhe des gedrehten Bildes übereinstimmt.", bufferedImage.getWidth(), editedImage.getHeight());
+
+		for (int i = 0; i < bufferedImage.getWidth(); i++) {
+			
+			for (int j = 0; j < bufferedImage.getHeight(); j++) {
+				
+				int originalPixel = bufferedImage.getRGB(i, j);
+				int editedPixel = editedImage.getRGB(j, editedImage.getHeight() - 1 - i);
+	
+				assertEquals("Stimmen die individuellen Pixel überein?", originalPixel, editedPixel);
+				
+			}
+			
+		}
+
+	}
 }
