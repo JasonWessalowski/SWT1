dcf3bd37abde58d7f0130fe7b1d90870021e1138
Jason Wessalowski Matrikelnummer 2048877 <jasonwessalowski@gmail.com>
2019-05-19 17:54:51 +0200

    Implementierung der Methode calculateWeights() in der Klasse HDRCombine.

--- iMage/iMage.HDrize/src/main/java/org/iMage/HDrize/HDRCombine.java
+++ iMage/iMage.HDrize/src/main/java/org/iMage/HDrize/HDRCombine.java
@@ -16,7 +16,32 @@ public class HDRCombine implements IHDRCombine {
 
   @Override
   public float[] calculateWeights() {
-    throw new UnsupportedOperationException("TODO Implement me!");
+
+	  float[] result = new float[256];
+	  
+	  // Set first and last value.
+	  result[0] = 1.0f;
+	  result[255] = 1.0f;
+	  
+	  for (int n = 1; n <= 235; n++) {
+		  
+		  // Set value of first and last 19 values based on recurrent equation. Rest is equal to 2.
+		  if (n < 20) {
+			  
+			  result[n] = result[n - 1] + (1 / 20);
+			  result[255 - n] = result[n];
+			  
+			  
+		  } else {
+			  
+			  result[n] = 2;
+			  
+		  }
+		  
+	  }
+	  
+	  return result;
+	  
   }
 
 }
