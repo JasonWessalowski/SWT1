674728a0b92157d55d34222a0cd39c1be9321ec5
Jason Wessalowski Matrikelnummer 2048877 <jasonwessalowski@gmail.com>
2019-05-26 15:01:13 +0200

    Implementierung von der Methode getPlugins der Klasse PluginManagement.

--- iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
+++ iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
@@ -1,5 +1,11 @@
 package org.iMage.plugins;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.ServiceLoader;
+
 /**
  * Knows all available plug-ins and is responsible for using the service loader API to detect them.
  *
@@ -22,8 +28,48 @@ public final class PluginManagement {
    * according to their class names.
    */
   public static Iterable<PluginForJmjrst> getPlugins() {
-    //TODO: implement me!
-    return null;
+    
+	  // Initialize new ServiceLoader.
+	  ServiceLoader<PluginForJmjrst> loader = ServiceLoader.load(PluginForJmjrst.class);
+	  // Prepare list.
+	  List<PluginForJmjrst> plugins = new ArrayList<PluginForJmjrst>();
+	  // Prepare iterator for list plugins.
+	  ListIterator<PluginForJmjrst> iterator;
+	  
+	  // Insert every plugin at its appropriate spot on the list.
+	  for (PluginForJmjrst plugin : loader) {
+		  
+		  // Start from the beginning of the list again.
+		  iterator = plugins.listIterator();
+		  // Save the current plugin for comparison.
+		  PluginForJmjrst current;
+		  
+		  // Look for appropriate spot for given plugin of the for-each loop.
+		  // Is there a next element?
+		  while (iterator.hasNext()) {
+			  
+			  // If yes, go to the next element in the list.
+			  current = iterator.next();
+			  
+			  // Does this current plugin have a longer name?
+			  if (plugin.compareTo(current) <= 0) {
+				  
+				  // If yes, go to the previous element and break the while loop.
+				  iterator.previous();
+				  break;
+				  
+			  }
+			  
+		  }
+		  
+		  // Add plugin of the current for-each iterator prior to the plugin with the longer name.
+		  iterator.add(plugin);
+		  
+	  }
+	  
+	  // Return finished list.
+	  return plugins;
+	  
   }
 
 }
