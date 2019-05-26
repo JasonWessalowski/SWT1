d4eb426de5e0a4c25e67b8732463b9484a0ef334
Jason Wessalowski Matrikelnummer 2048877 <jasonwessalowski@gmail.com>
2019-05-26 20:15:58 +0200

    Implementierung der getName und isConfigurable Methoden in der Klasse Instagrim.

--- iMage/iMage.Instagrim-plugin/src/main/java/org/iMage/Instagrim/Instagrim.java
+++ iMage/iMage.Instagrim-plugin/src/main/java/org/iMage/Instagrim/Instagrim.java
@@ -1,6 +1,41 @@
 package org.iMage.Instagrim;
 
+import org.iMage.plugins.PluginForJmjrst;
+import org.jis.Main;
+
 public class Instagrim extends PluginForJmjrst {
+
+	@Override
+	public void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		
+		return "Instagrim";
+		
+	}
+
+	@Override
+	public void init(Main arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isConfigurable() {
+		
+		return true;
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 
